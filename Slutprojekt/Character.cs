8f3faeadd43a757b0b94e090214c0dc266657f71
Aris Paschalidis <aris.paschalidis@elev.ga.ntig.se>
2026-05-08 13:34:46 +0200

    dwadawd

--- Slutprojekt/Character.cs
+++ Slutprojekt/Character.cs
@@ -2,7 +2,21 @@ using System;
 
 namespace Slutprojekt;
 
-public class Character
+public abstract class Character
 {
+public string Name;
+public int Health;
+public int MaxHealth;
 
+public Character(string name, int hp)
+    {
+        Name = name;
+        Health = hp;
+        MaxHealth = hp;
+    }
+    public bool IsAlive() {return Health > 0;}
+
+    public void TakingDamage(int amount)
+    {
+    }
 }
