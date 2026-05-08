34f0529f388c5b5238d37da9a8a210da2dda0dac
Aris Paschalidis <aris.paschalidis@elev.ga.ntig.se>
2026-05-08 14:18:06 +0200

    wdwaadawdwadaw

--- Slutprojekt/Player.cs
+++ Slutprojekt/Player.cs
@@ -7,4 +7,13 @@ public class Player : Character
    public int Gold;
    public List<string> Inventory = new List<string>();
    
+   public Player(string name) : base(name, 100) {Gold = 100}
+
+    public override void Attack(Character target)
+    {
+    Console.WriteLine(Name + "attackerar " + target.Name + "!");
+    target.TakingDamage(10);
+    }
+
+
 }
