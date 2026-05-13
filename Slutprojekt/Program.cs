333c745b90bc0a3a9f97e7def6e0fc5342f591c2
Aris Paschalidis <aris.paschalidis@elev.ga.ntig.se>
2026-05-13 14:33:05 +0200

    wdwadafasdf

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine ("Namn");
+﻿using Slutprojekt;
+
+class program
+{
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Namn: ");
+        string namn = Console.ReadLine();
+        if (namn == "") namn = "Hjälten";
+
+        Player player = new Player(namn);
+        Enemy fiende = new Goblin();
+
+        Console.WriteLine("Välkommen " + player.Name + "!");
+
+        while (player.IsAlive() && fiende.IsAlive())
+        {
+            
+        } 
+    }
+}
