2f985fb395afc8e8f315a5334f809fb5e9f2a1fe
Aris Paschalidis <aris.paschalidis@elev.ga.ntig.se>
2026-05-13 13:37:52 +0200

    wdwadgfasgf

--- Slutprojekt/Goblin.cs
+++ Slutprojekt/Goblin.cs
@@ -2,7 +2,17 @@ using System;
 
 namespace Slutprojekt;
 
-public class Goblin
+public class Goblin : Enemy
 {
+ public Goblin() : base("Goblin", 25, 4, 8)
+    {
+        
+    }
 
+    public override void Attack(Character target)
+    {
+        Console.WriteLine(Name + "attackerar två gånger!");
+        target.TakingDamage(Damage);
+        target.TakingDamage(Damage);
+    }
 }
